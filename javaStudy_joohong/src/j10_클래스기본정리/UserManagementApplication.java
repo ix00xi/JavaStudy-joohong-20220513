b0ac1aadb72fc390fd9172827b2be1728d213af3
ix00xi <izx43@naver.com>
2022-05-25 21:57:58 +0900

    20220525 자바 수업 내용

---
+++ javaStudy_joohong/src/j10_클래스기본정리/UserManagementApplication.java
@@ -0,0 +1,16 @@
+package j10_클래스기본정리;
+
+import j10_클래스기본정리.controller.UserManagementController;
+import j10_클래스기본정리.service.UserService;
+
+public class UserManagementApplication {
+
+	public static void main(String[] args) {
+		
+		UserManagementController userManagementController 
+		= new UserManagementController(new UserService());
+		userManagementController.createUser();
+
+	}
+
+}
---
+++ javaStudy_joohong/src/j10_클래스기본정리/controller/UserManagementController.java
@@ -0,0 +1,25 @@
+package j10_클래스기본정리.controller;
+
+import j10_클래스기본정리.repository.User;
+import j10_클래스기본정리.service.UserService;
+
+public class UserManagementController {
+	
+	private UserService userService;
+	
+	public UserManagementController(UserService userService) {
+		this.userService = userService;
+	}
+
+	public void createUser() {
+		User user = userService.insertUser();
+		
+		System.out.println("[새로 추가된 사용자]");
+		System.out.println(user.toString());
+	}
+	
+	public void updateUser() {
+		userService.insertUser();
+	}
+	
+}
